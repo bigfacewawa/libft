a403439f23d335a20719d921cea6a238e6b86294
Yijing Huang <yijhuang@e1z2r5p46.42.us.org>
2018-12-31 15:31:05 -0800

    norm trim&is_lower

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,51 +6,36 @@
 /*   By: yijhuang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/31 00:56:14 by yijhuang          #+#    #+#             */
-/*   Updated: 2018/12/31 00:57:00 by yijhuang         ###   ########.fr       */
+/*   Updated: 2018/12/31 15:29:54 by yijhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s)
+char	*ft_strtrim(char const *s)
 {
-	char *str;
-	int i;
-	int len;
-	int j;
+	char	*str;
+	int		i;
+	int		j;
+	int		len;
 
 	if (!s)
-	{
 		return (NULL);
-	}
 	i = 0;
 	j = 0;
 	len = ft_strlen(s);
 	while (ft_is_space(*(s + i)))
-	{
 		i++;
-	}
 	while (ft_is_space(*(s + len - 1)))
-	{
 		len--;
-	}
 	if (len - i > 0)
-	{
 		str = (char*)ft_memalloc(len - i + 1);
-	}
 	else
-	{
 		str = (char*)ft_memalloc(1);
-	}
 	if (!str)
-	{
-			return (NULL);
-	}
-	while (j < len - i)
-	{
+		return (NULL);
+	while (j++ < len - i)
 		str[j] = s[i + j];
-		j++;
-	}
 	str[j] = '\0';
 	return (str);
 }
