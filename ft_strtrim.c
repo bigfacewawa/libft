e0e6a0f430f848250b5396e2832625918607e0fd
Yijing Huang <yijhuang@e1z3r4p15.42.us.org>
2018-12-31 00:31:09 -0800

    modified trim only macro

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,36 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   trim only macro.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yijhuang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/24 21:06:47 by yijhuang          #+#    #+#             */
-/*   Updated: 2018/12/30 01:33:12 by yijhuang         ###   ########.fr       */
+/*   Created: 2018/12/31 00:27:40 by yijhuang          #+#    #+#             */
+/*   Updated: 2018/12/31 00:29:14 by yijhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char *ft_strtrim(char const *s)
 {
-	char	*str;
-	int		i;
-	int		len;
-	int		j;
+	char *str;
+	int i;
+	int len;
+	int j;
 
 	NULL_CHECK(!s);
 	i = 0;
 	j = 0;
 	len = ft_strlen(s);
 	while (ft_is_space(*(s + i)))
+	{
 		i++;
+	}
 	while (ft_is_space(*(s + len - 1)))
+	{
 		len--;
+	}
 	if (len - i > 0)
+	{
 		str = (char*)ft_memalloc(len - i + 1);
+	}
 	else
+	{
 		str = (char*)ft_memalloc(1);
+	}
 	NULL_CHECK(!str);
 	while (j < len - i)
 	{
