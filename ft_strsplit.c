b082ec41d11b64ba89ae75bf3138d98328ce2c1e
Yijing Huang <yijhuang@e1z3r4p15.42.us.org>
2018-12-31 00:47:13 -0800

    trim&split back

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   split only macro.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yijhuang <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/31 00:33:55 by yijhuang          #+#    #+#             */
-/*   Updated: 2018/12/31 00:39:53 by yijhuang         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 char	**ft_strsplit(char const *s, char c)
@@ -25,7 +13,10 @@ char	**ft_strsplit(char const *s, char c)
 	num = ft_words_count(s, c);
 
 	str = (char**)malloc((num + 1) * sizeof(char*));
-	NULL_CHECK(!str);
+	if (!str)
+	{
+		return (NULL);
+	}
 	else if (num == 0)
 	{
 		str[0] = 0;
@@ -41,11 +32,17 @@ char	**ft_strsplit(char const *s, char c)
 			}
 			len = ft_word_len((s + i), c);
 			str[j] = ft_strsub((s + i), 0, len);
-			NULL_CHECK(!str[j]);
-			i = i + len;
-			j++;
+			if (!str[j])
+			{
+				return (NULL);
+			}
+			else
+			{
+				i = i + len;
+				j++;
+			}
 		}
-		str[j] = '\0';
+		str[j] = NULL;
 		return (str);
 	}
 }
