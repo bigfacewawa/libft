2c9539b778d2ff08cdb839309987b883992d37eb
Yijing Huang <yijhuang@e1z2r5p46.42.us.org>
2018-12-31 15:55:36 -0800

    norm split except too much lines

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: yijhuang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/31 00:57:28 by yijhuang          #+#    #+#             */
-/*   Updated: 2018/12/31 00:58:08 by yijhuang         ###   ########.fr       */
+/*   Updated: 2018/12/31 15:54:30 by yijhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,16 @@ char	**ft_strsplit(char const *s, char c)
 {
 	int		i;
 	int		j;
-	int 	num;
-	int 	len;
+	int		num;
+	int		len;
 	char	**str;
 
 	i = 0;
 	j = 0;
 	num = ft_words_count(s, c);
-
 	str = (char**)malloc((num + 1) * sizeof(char*));
 	if (!str)
-	{
 		return (NULL);
-	}
 	else if (num == 0)
 	{
 		str[0] = 0;
@@ -39,15 +36,11 @@ char	**ft_strsplit(char const *s, char c)
 		while (num--)
 		{
 			while (*(s + i) == c && *(s + i) != '\0')
-			{
 				i++;
-			}
 			len = ft_word_len((s + i), c);
 			str[j] = ft_strsub((s + i), 0, len);
 			if (!str[j])
-			{
 				return (NULL);
-			}
 			else
 			{
 				i = i + len;
